7e51a9b7b9f8dba0f6d76f78557bb88ca7e0f87a
coal1633 <32506489+coal1633@users.noreply.github.com>
2018-10-05 14:18:26 +0200

    first other file

--- app.js
+++ app.js
@@ -1,84 +1,22 @@
 const express = require('express')
-const sqlite3 = require('sqlite3')
 const bodyParser = require('body-parser')
+require('body-parser-xml')(bodyParser)
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
-const db = new sqlite3.Database("database.db")
-
-db.run("PRAGMA foreign_keys = ON")
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS Company (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name VARCHAR(20) UNIQUE,
-		hashedPassword VARCHAR(15),
-		location TEXT
-	)
-`)
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS User (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username VARCHAR(20) UNIQUE,
-		hashedPassword VARCHAR(15)
-	)
-`)
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS Advert (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		company_id INTEGER,
-		title TEXT,
-		sector VARCHAR(30),
-		type TEXT,
-		description TEXT,
-		location VARCHAR(50),
-		FOREIGN KEY(\`company_id\`) REFERENCES \`Company\`(\`id\`) ON DELETE CASCADE
-	)
-`)
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS Skill (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		skill_name VARCHAR(20) UNIQUE
-	)
-`)
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS UserSkill (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		skill_id INTEGER,
-		user_id INTEGER,
-		FOREIGN KEY(\`skill_id\`) REFERENCES \`Skill\`(\`id\`) ON DELETE CASCADE,
-		FOREIGN KEY(\`user_id\`) REFERENCES \`User\`(\`id\`) ON DELETE CASCADE
-	)
-`)
-db.run(`
-  CREATE TABLE IF NOT EXISTS AdvertSkill (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		skill_id INTEGER,
-		advert_id INTEGER,
-		FOREIGN KEY(\`skill_id\`) REFERENCES \`Skill\`(\`id\`) ON DELETE CASCADE,
-		FOREIGN KEY(\`advert_id\`) REFERENCES \`Advert\`(\`id\`) ON DELETE CASCADE
-	)
-`)
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS Application(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		user_id INTEGER,
-		company_id INTEGER,
-		advert_id INTEGER,
-		FOREIGN KEY(\`user_id\`) REFERENCES \`User\`(\`id\`) ON DELETE CASCADE,
-		FOREIGN KEY(\`company_id\`) REFERENCES \`Company\`(\`id\`) ON DELETE CASCADE,
-		FOREIGN KEY(\`advert_id\`) REFERENCES \`Advert\`(\`id\`) ON DELETE CASCADE
-	)
-`)
+require('./database-structure')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(bodyParser.xml({
+  limit: '1MB',   // Reject payload bigger than 1 MB
+  xmlParseOptions: {
+    normalize: true,     // Trim whitespace inside text nodes
+    normalizeTags: true, // Transform tags to lowercase
+    explicitArray: false // Only put nodes in array if >1
+  }
+}))
 app.use(bodyParser.urlencoded({extended: false}))
 const jwtSecret = "dsjlksdjlkjfdsl"
 
@@ -139,8 +77,7 @@ app.get("/adverts", function(req, res){
 	let sector = req.query.sector
 	let location  = req.query.location
 	let type = req.query.type
-	let contentType = res.headers['content-type'];
-
+	let contentType = req.headers['content-type'];
 		if(title){
 			query+=" title = ? AND"
 			values.push(title.toLowerCase())
